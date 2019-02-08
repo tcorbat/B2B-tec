24982e5595db3e103f5df53c802d7cae36b3fa06
tcorbat <thomas.corbat@gmail.com>
2019-02-08 09:12:51 +0100

    reorder code in ProfileTabBase

--- week 3/solution/src/ch/b2btec/ui/generated/ProfileTabBase.java
+++ week 3/solution/src/ch/b2btec/ui/generated/ProfileTabBase.java
@@ -1,7 +1,11 @@
 package ch.b2btec.ui.generated;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Insets;
 
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.TitledBorder;
@@ -9,10 +13,6 @@ import javax.swing.border.TitledBorder;
 import ch.b2btec.bl.domain.Customer;
 import ch.b2btec.ui.AddressPanel;
 import ch.b2btec.ui.models.AddressModel;
-import javax.swing.JButton;
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
 
 public class ProfileTabBase extends JPanel {
 	private static final long serialVersionUID = -3501323630210245255L;
@@ -22,22 +22,20 @@ public class ProfileTabBase extends JPanel {
 	protected final JButton changePasswordButton;
 
 	protected ProfileTabBase(Customer customer) {
-		
-		JPanel panel = new JPanel();
-		add(panel);
+		JPanel userPanel = new JPanel();
 		GridBagLayout gbl_panel = new GridBagLayout();
 		gbl_panel.columnWidths = new int[] {0, 0, 0};
 		gbl_panel.rowHeights = new int[]{55, 55, 0};
 		gbl_panel.columnWeights = new double[]{0.0, Double.MIN_VALUE};
 		gbl_panel.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
-		panel.setLayout(gbl_panel);
+		userPanel.setLayout(gbl_panel);
+		
 		var detailsPanel = new JPanel();
 		GridBagConstraints gbc_detailsPanel = new GridBagConstraints();
 		gbc_detailsPanel.fill = GridBagConstraints.BOTH;
 		gbc_detailsPanel.insets = new Insets(0, 0, 5, 0);
 		gbc_detailsPanel.gridx = 0;
 		gbc_detailsPanel.gridy = 0;
-		panel.add(detailsPanel, gbc_detailsPanel);
 		detailsPanel.setBorder(new TitledBorder("User Details"));
 		detailsPanel.setLayout(new GridLayout(2, 2, 5, 5));
 		detailsPanel.add(new JLabel("Customer Name:"));
@@ -46,12 +44,16 @@ public class ProfileTabBase extends JPanel {
 		detailsPanel.add(new JLabel("Business Number:"));
 		businessNumberlabel = new JLabel(Integer.toString(customer.getBusinessNumber()));
 		detailsPanel.add(businessNumberlabel);
+		userPanel.add(detailsPanel, gbc_detailsPanel);
 		
 		changePasswordButton = new JButton("Change Password");
+		
 		GridBagConstraints gbc_changePasswordButton = new GridBagConstraints();
 		gbc_changePasswordButton.gridx = 0;
 		gbc_changePasswordButton.gridy = 1;
-		panel.add(changePasswordButton, gbc_changePasswordButton);
+		userPanel.add(changePasswordButton, gbc_changePasswordButton);
+
+		add(userPanel);
 
 		var deliveryAddress = new AddressPanel(new AddressModel(customer.getProfile().getDeliveryAddress()));
 		deliveryAddress.setBorder(new TitledBorder("Delivery Address"));
